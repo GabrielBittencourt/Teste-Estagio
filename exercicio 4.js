ea3929748404ea6f3b8375feaebdd405418bf1ac
GabrielBittencourt <gdsbittencourt@inf.ufpel.edu.br>
2020-02-27 22:52:38 -0300

    Necessita correções

---
+++ exercicio 4.js
@@ -0,0 +1,8 @@
+var teste = "teste 1 de 2 string 3";
+
+
+teste = teste.replace(/1/g, "[removido]");
+teste = teste.replace(/2/g, "[removido]");
+teste = teste.replace(/3/g, "[removido]");
+
+console.log(teste);
