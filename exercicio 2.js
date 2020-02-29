ff24f1f470424f60d2be9c55dd03efe0c150a166
GabrielBittencourt <gdsbittencourt@inf.ufpel.edu.br>
2020-02-29 00:23:04 -0300

    Adicionados comentários

--- exercicio 2.js
+++ exercicio 2.js
@@ -1,6 +1,8 @@
-var a = "abc";
-var b = "abcd";
+//criação das variáveis
+let a = "abc";
+let b = "abcd";
 
+//compara as duas string e retorna a maior
 function compara(a,b){
     if(a > b){
         return a;
@@ -9,8 +11,10 @@ function compara(a,b){
     }
 }
 
+//função que chama a função de comparação utilizando duas strings
 function aplica_funcao(compara, a, b){
     compara(a,b);
 }
 
+//imprime o resultado
 console.log(aplica_funcao(compara, a, b));
