e6695444a3146b196dea39e04329d6b5d40771b5
GabrielBittencourt <gdsbittencourt@inf.ufpel.edu.br>
2020-02-29 00:23:55 -0300

    Adicionados comentários

--- exercicio 3.js
+++ exercicio 3.js
@@ -1,11 +1,13 @@
+//criação das variáveis
 var a = "abc";
 var b = "abcd";
 var c = "123";
 var d = "1234";
 
+//concatena todas as strings e retorna a string resultante
 function concat(a,b,c,d){
     return a+b+c+d;
 }
     
-
+//imprime o resultado
 console.log(concat(a,b,c,d));
